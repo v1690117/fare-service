f7282c1ca44c1079784305e4476c124a57074d62
v1690117 <v1690117@outlook.com>
2025-02-05 15:13:10 +0300

    add saving processing file

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ import pickle
 import face_recognition
 import numpy as np
 from flask import Flask, request, jsonify
+import os
+import random
+import string
 from asgiref.wsgi import WsgiToAsgi
 
 with open("model.pkl", "rb") as file:
@@ -11,12 +14,22 @@ with open("model.pkl", "rb") as file:
 app = Flask(__name__)
 asgi_app = WsgiToAsgi(app)
 
+def generate_random_string(length=10):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(length))
+    return random_string
+
 @app.route("/recognize", methods=["POST"])
 def recognize():
     if "file" not in request.files:
         return jsonify({"error": "No file provided"}), 400
     file = request.files["file"]
-    image = face_recognition.load_image_file(file)
+
+
+    file_path = os.path.join(".", f"{generate_random_string(16)}-{file.filename}")
+    file.save(file_path)
+
+    image = face_recognition.load_image_file(file_path)
 
     result = []
     face_encodings = face_recognition.face_encodings(image)
